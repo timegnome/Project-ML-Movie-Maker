65bf1ccb1ac11493dbaf49a3d7e4383bd843d345
Kirstie Jones <kirstiekjones@gmail.com>
2020-11-17 15:15:46 -0800

    uploading Flask app components

---
+++ FlaskApp/response.py
@@ -0,0 +1,38 @@
+import urllib.request
+import os
+import json
+
+data = {
+    "Inputs": {
+
+            "input1":
+            {
+                "ColumnNames": ["Budget", "Keywords", "Genre", "Vote Average"],
+                "Values": [ [ "0", "0", "22", "0" ], [ "0", "0", "3", "0" ], ]
+            },
+    },
+        "GlobalParameters": {
+
+        }
+}
+
+body = str.encode(json.dumps(data))
+url = os.environ.get('URL', '<URL>')
+api_key = os.environ.get('API_KEY', '<API_KEY>')
+headers = {'Content-Type': 'application/json', 'Authorization': ('Bearer ' + api_key)}
+
+
+req = urllib.request.Request(url, body, headers)
+print(req)
+try:
+    response = urllib.request.urlopen(req)
+
+    result = response.read()
+    print(result)
+
+except urllib.error.HTTPError as error:
+    print("The request failed with status code: " + str(error.code))
+
+    print(error.info())
+
+    print(json.loads(error.read()))
